719c2a9db60f13cd6bba4e37f64ed9c3930718a8
Andres Laverde <jalpgj1998@gmail.com>
2023-08-14 23:00:11 -0500

    Cuarto avance... noche

--- index.js
+++ index.js
@@ -4,17 +4,32 @@ document.addEventListener("DOMContentLoaded", function() {
         var header = document.getElementById("header")
         var headerLogo = document.getElementById("header__logo")
         var header__boton__burger = document.getElementById("header__boton__burger")
+        var menu__desktop__a1 = document.getElementById("menu__desktop__a1")
+        var menu__desktop__a2 = document.getElementById("menu__desktop__a2")
+        var menu__desktop__a3 = document.getElementById("menu__desktop__a3")
+        var menu__desktop__a4 = document.getElementById("menu__desktop__a4")
+        
         var scrollY = window.scrollY
     if(Xactive==true){
         if(scrollY>0 ){
             header.style.backgroundColor = "white"
             headerLogo.style.color = "black"
             header__boton__burger.setAttribute("src", "./img/burgerblack.png")
+            menu__desktop__a1.style.color = "black";
+            menu__desktop__a2.style.color = "black";
+            menu__desktop__a3.style.color = "black";
+            menu__desktop__a4.style.color = "black";
+            
         }
         else{
             header.style.backgroundColor = "transparent"
             headerLogo.style.color = "white"
             header__boton__burger.setAttribute("src", "./img/burgerwhite.png")
+            menu__desktop__a1.style.color = "white";
+            menu__desktop__a2.style.color = "white";
+            menu__desktop__a3.style.color = "white";
+            menu__desktop__a4.style.color = "white";
+            
         }
     }else{
         header.style.backgroundColor = "white"
